17deb120e66d2eecb48a29202bd8c6c39ca3a9c6
Jhonel Villanueva <vvjhonel@gmail.com>
2022-12-20 20:27:42 +0800

    Add bubble_sort method

---
+++ bubble-sort/bubble_sort.rb
@@ -0,0 +1,28 @@
+def bubble_sort(array)
+  
+  k = 1
+  i = 0
+  while (i < array.length - 1) do
+      
+    sorted = array.each_with_index do |num, i|
+      if i < array.length - k
+        temp = 0
+        if num > array[i + 1]
+          temp = array[i + 1]
+          array[i + 1] = num
+          array[i] = temp
+        end
+      end
+    end
+
+    k += 1
+    i +=1
+
+  end
+  
+  sorted
+
+end
+
+p bubble_sort([3, 2, 1])
+# [0, 2, 2, 3, 4, 78]
