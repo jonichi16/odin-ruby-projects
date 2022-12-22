54751df4f2b463466490b040743a1e888db01ee4
Jhonel Villanueva <vvjhonel@gmail.com>
2022-12-22 22:04:16 +0800

    Add unique character check

--- tic-tac-toe/tic_tac_toe.rb
+++ tic-tac-toe/tic_tac_toe.rb
@@ -16,9 +16,9 @@ module Board
   def create_board(player1, player2)
     puts "\t 1 | 2 | 3 \t\tScores:"
     puts "\t---+---+---"
-    puts "\t 4 | 5 | 6 \t\t#{player1.name} Score: #{player1.score}"
+    puts "\t 4 | 5 | 6 \t\t#{player1.name} (#{player1.character}) Score: #{player1.score}"
     puts "\t---+---+---"
-    puts "\t 7 | 8 | 9 \t\t#{player2.name} Score: #{player2.score}"
+    puts "\t 7 | 8 | 9 \t\t#{player2.name} (#{player2.character}) Score: #{player2.score}"
   end
 
   def create_players(num)
@@ -27,7 +27,15 @@ module Board
     puts "What is #{player_name}'s character?"
     player_character = gets.chomp
     player = Player.new(player_name, player_character)
+    unique_character(player) unless players[0].nil?
     add_player(player)
+  end
+
+  def unique_character(player)
+    while players[0].character == player.character
+      puts "#{player.character} is already taken. Please choose another"
+      player.character = gets.chomp
+    end
   end
 end
 
@@ -55,18 +63,20 @@ class Game
     (1..2).each { |num| create_players(num) }
     puts "\n\n"
     create_board(players[0], players[1])
+    p players
   end
 end
 
 # * This will create a player
 class Player
   include Board
-  attr_accessor :name, :character, :score
+  attr_accessor :name, :character, :score, :player_move
 
   def initialize(name, character)
     self.name = name
     self.character = character
     self.score = 0
+    self.player_move = []
   end
 end
 
