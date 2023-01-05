8ae3abdbcbb8fe5e2a269b7b34d1444c9987b6ab
Jhonel Villanueva <vvjhonel@gmail.com>
2023-01-06 02:50:02 +0800

    Add game display

--- hangman-game/lib/game.rb
+++ hangman-game/lib/game.rb
@@ -5,16 +5,41 @@ require './lib/words'
 # *Class for creating the game
 class Game
   include SelectableWords
-  attr_accessor :word
+  attr_accessor :word, :wordline, :remaining_guess, :used_letters
 
   def initialize
     self.word = select_word
+    self.wordline = create_wordline
+    self.remaining_guess = 5
+    self.used_letters = []
   end
 
   def select_word
     WORDS[rand(0...WORDS.length)]
   end
+
+  def create_wordline
+    Array.new(word.length).map { '_' }
+  end
+
+  def start
+    puts File.read('rules.txt')
+    action = gets.chomp
+    display_game if action
+  end
+
+  def display_game
+    puts "\n\n"
+    puts ' HANGMAN '.center(50, '=')
+    puts "Remaining guess: #{remaining_guess}"
+    puts "Used letter: #{used_letters}\n\n"
+    display_wordline
+  end
+
+  def display_wordline
+    puts wordline.join(' ')
+  end
 end
 
-hangman = Game.new
-puts hangman.word
+game = Game.new
+game.start
