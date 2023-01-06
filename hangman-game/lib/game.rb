d448102278d535afff6f5b66e22a9698c1b8d98b
Jhonel Villanueva <vvjhonel@gmail.com>
2023-01-06 14:37:29 +0800

    Base game without save

--- hangman-game/lib/game.rb
+++ hangman-game/lib/game.rb
@@ -5,13 +5,14 @@ require './lib/words'
 # *Class for creating the game
 class Game
   include SelectableWords
-  attr_accessor :word, :wordline, :remaining_guess, :used_letters
+  attr_accessor :word, :wordline, :remaining_guess, :used_letters, :all_used_letters
 
   def initialize
     self.word = select_word
     self.wordline = create_wordline
     self.remaining_guess = 5
     self.used_letters = []
+    self.all_used_letters = []
   end
 
   def select_word
@@ -25,7 +26,8 @@ class Game
   def start
     puts File.read('rules.txt')
     action = gets.chomp
-    display_game if action
+    player_guess if action
+    game_result
   end
 
   def display_game
@@ -33,11 +35,50 @@ class Game
     puts ' HANGMAN '.center(50, '=')
     puts "Remaining guess: #{remaining_guess}"
     puts "Used letter: #{used_letters}\n\n"
-    display_wordline
+    puts wordline.join(' ')
   end
 
-  def display_wordline
-    puts wordline.join(' ')
+  def player_guess
+    until gameover
+      display_game
+      print "\nEnter your guess:"
+      guess = gets.chomp.downcase
+      if valid_guess(guess)
+        puts "\nInvalid guess. Try Again"
+      else
+        check_guess(guess)
+      end
+    end
+  end
+
+  def check_guess(guess)
+    if word.include?(guess)
+      word.split('').each_with_index do |char, i|
+        wordline[i] = guess if char == guess
+      end
+      all_used_letters.push(guess)
+    else
+      used_letters.push(guess)
+      all_used_letters.push(guess)
+      self.remaining_guess -= 1
+    end
+  end
+
+  def game_result
+    display_game
+    if word == wordline.join('')
+      puts 'You WON!'
+    else
+      puts "You LOSE! The word is #{word}"
+    end
+  end
+
+  def gameover
+    remaining_guess.zero? || word == wordline.join('')
+  end
+
+  def valid_guess(guess)
+    guess.length > 1 || guess.match?(/[^a-z]/) || all_used_letters.include?(guess)
   end
 end
 
--- hangman-game/lib/words.rb
+++ hangman-game/lib/words.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module SelectableWords
-  WORDS = File.readlines('google_words.txt').each(&:strip!).filter { |word| word.length.between?(5, 12)}
+  WORDS = File.readlines('google_words.txt').each(&:strip!).filter { |word| word.length.between?(5, 12) }
 end
