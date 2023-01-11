c55c74b9e0aaf8882a3f20076f0618915b5ac5f8
Jhonel Villanueva <vvjhonel@gmail.com>
2023-01-12 03:17:00 +0800

    Add rebalance method

--- binary-search-trees/lib/main.rb
+++ binary-search-trees/lib/main.rb
@@ -5,14 +5,13 @@ require './lib/tree'
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 tree = Tree.new(array)
 
-tree.insert(116)
-tree.find(324)
-tree.insert(2)
-tree.delete(4)
+tree.insert(123)
+tree.insert(174)
+tree.insert(157)
 tree.pretty_print
-# level_order_arr = []
-# tree.level_order { |item| level_order_arr.push(item) }
-# p level_order_arr
-inorder_arr = []
-tree.inorder { |item| inorder_arr.push(item) }
-p inorder_arr
+p tree.height(8)
+p tree.depth(1)
+p tree.balanced?
+tree.rebalance
+p tree.balanced?
+tree.pretty_print
