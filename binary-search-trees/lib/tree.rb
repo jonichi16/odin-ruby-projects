a29915f57a5b36c7e85a8e3f958be5fbe40c1359
Jhonel Villanueva <vvjhonel@gmail.com>
2023-01-11 13:21:14 +0800

    Add delete method

--- binary-search-trees/lib/tree.rb
+++ binary-search-trees/lib/tree.rb
@@ -23,14 +23,41 @@ class Tree
 
   def insert(value, current = root, prev = nil)
     if current.nil?
-      if value < prev.data
-        prev.left = Node.new(value)
-      else
-        prev.right = Node.new(value)
-      end
-      return
+      return prev.left = Node.new(value) if value < prev.data
+
+      return prev.right = Node.new(value)
+
     end
 
+    return current if current.data == value
+
     value < current.data ? insert(value, current.left, current) : insert(value, current.right, current)
+  end
+
+  def delete(value, current = root)
+    return current if current.nil?
+
+    if value < current.data
+      current.left = delete(value, current.left)
+    elsif value > current.data
+      current.right = delete(value, current.right)
+    else
+      return current.right if current.left.nil?
+      return current.left if current.right.nil?
+
+      current.data = min_value(current.right)
+      current.right = delete(current.data, current.right)
+    end
+
+    current
+  end
+
+  def min_value(root)
+    min = root.data
+    until root.left.nil?
+      min = root.left.data
+      root = root.left
+    end
+    min
   end
 end
